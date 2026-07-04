b942f0f3cb0c76ce7a9ab986520943b4a7a18ea8
maseera_09 <maseera169@gmail.com>
2026-07-04 11:15:36 +0530

    Time: 4 ms (31.72%) | Memory: 8.8 MB (7.76%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -3,19 +3,20 @@ public:
     string mergeAlternately(string word1, string word2) {
        int i=0,j=0;
        string str;
+       str.reserve(word1.size()+word2.size());
        int n=word1.size(),m=word2.size();
        while(i<n && j<m){
-str+=word1[i];
-str+=word2[j];
-i++,j++;
+str+=word1[i++];
+str+=word2[j++];
+
        }
        while(j==m && i<n){
-        str+=word1[i];
-        i++;
+        str+=word1[i++];
+        
     }
        while(i==n && j<m){
-        str+=word2[j];
-        j++;
+        str+=word2[j++];
+        
     }
     
     return str;}
