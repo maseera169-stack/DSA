af9199ec22cc5e50bcd2b9f5c769a4e6c0dc44aa
maseera_09 <maseera169@gmail.com>
2026-09-03 21:01:11 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (93.19%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -8,36 +8,52 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution{
 public:
 
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-       ListNode* temp;
+//        ListNode* temp;
+//        ListNode* prev=NULL;
+//        ListNode* current=head;
+       
+//        int cnt=0,cnt2=0,val=0;
+// temp=head;
+
+//        while(temp!=NULL){
+//         temp=temp->next;
+//         cnt++;
+//        }
+    //    if(cnt==1) return NULL;
+       
+//        val=cnt-n;
+//        if(val==0){
+// head=head->next;
+// return head;
+//        }
+
+// while(current!=NULL && current->next!=NULL && cnt2!=val){
+// prev=current;
+// current=current->next;
+// cnt2++;
+// }
+// prev->next=current->next;
+
+
+
+       ListNode* fast=head;
        ListNode* prev=NULL;
-       ListNode* current=head;
-       
-       int cnt=0,cnt2=0,val=0;
-temp=head;
-
-       while(temp!=NULL){
-        temp=temp->next;
-        cnt++;
-       }
-       if(cnt==1) return NULL;
-       
-       val=cnt-n;
-       if(val==0){
-head=head->next;
-return head;
-       }
-
-while(current!=NULL && current->next!=NULL && cnt2!=val){
-prev=current;
-current=current->next;
-cnt2++;
+       ListNode* slow=head;
+       for(int i=0;i<n;i++){
+    fast=fast->next;
 }
-prev->next=current->next;
+       while(fast!=NULL){
+        prev=slow;
+        slow=slow->next;
+        fast=fast->next;
+}
+       if(prev==NULL && fast==NULL) return head=head->next; 
+       prev->next=slow->next;  
+       return head; }
+       
 
-
-    return head;}
 };
