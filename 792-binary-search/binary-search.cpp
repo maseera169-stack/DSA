3ecef84c693deb7473c5a9826df147b918cc26e9
maseera_09 <maseera169@gmail.com>
2026-09-11 22:37:42 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (81.16%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -4,15 +4,15 @@ public:
         int low=0,high=nums.size()-1;
 
         while(low<=high){
-int mid=low+high/2;
+int mid=low+(high-low)/2;
 
 if(nums[mid]==target){
     return mid;
 }
 else if(nums[mid]>target){
-    high--;
+    high=mid-1;
 }
-else low++;
+else low=mid+1;;
         }
     return -1;}
 };
