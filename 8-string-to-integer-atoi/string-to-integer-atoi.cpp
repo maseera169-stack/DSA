9b2969cf8ba0f6693611d9a29c51cf40ef84b29f
maseera_09 <maseera169@gmail.com>
2026-09-14 13:16:16 +0530

    Time: 3 ms (12.83%) | Memory: 10.4 MB (5.31%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
+long long solve(string s,int ind,long long num){
+    if(ind==s.size()|| s[ind]<'0' || s[ind]>'9'){
+        return num;
+    }
+    num=num*10+(s[ind]-'0');
+     if(num > INT_MAX)
+            return num;
+    return solve(s,ind+1,num);
+
+
+}
     int myAtoi(string s) {
 int i=0;
 int sign=1;
-long long num=0;
+long long no=0;
 
 int n=s.size();
 
@@ -19,18 +30,23 @@ i++;}
         
         }
 
-        while(i<n && s[i]>='0' && s[i]<='9'){
-num=num*10+(s[i]-'0');
+//         while(i<n && s[i]>='0' && s[i]<='9'){
+// num=num*10+(s[i]-'0');
 
         
         
         
-        if(sign==1 && num > INT_MAX)
+//         if(sign==1 && num > INT_MAX)
+//             return INT_MAX;
+//          if(sign==-1 && -num < INT_MIN)
+//             return INT_MIN;
+
+//  i++;  }
+long long num=solve(s,i,no);
+         if(sign==1 && num > INT_MAX)
             return INT_MAX;
-         if(sign==-1 && num > 2147483647LL)
+         if(sign==-1 && -num < INT_MIN)
             return INT_MIN;
 
- i++;           
-        }
     return sign*num;}
 };
